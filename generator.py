2586690cdff7bb1f0dcad12ad0cd6aab80c028f5
Bob Lien <boblien0818@gmail.com>
2024-01-10 00:28:16 +0800

    add progress bar

--- generator.py
+++ generator.py
@@ -4,27 +4,68 @@ from docx2pdf import convert
 import os
 import pandas as pd
 import tkinter as tk
+import threading
 from tkinter import filedialog as fd
+from tkinter import ttk
 # ---imports---
 
 # ---variables---
+num = 0
 p_numbers = 0
 placeholders = []
 p_title = []
 p_paragraph = []
 save_with = ""
 save_name = ""
-save_folder = "./"
+save_folder = "."
 doc_path = ""
 data_path = ""
 # ---variables---
 
-# ---generating functions---
-def export(pdf):
-    global p_numbers, p_paragraph, placeholders, p_title, save_with, save_name, save_folder, doc_path, data_path, output
-    doc = docx.Document(doc_path)
+# ---new window function---
+def new():
+    # ---close window function---
+    def close():
+        root.quit()
+        return None
+    # ---close window function---
+    # ---check function---
+    def check_thread():
+        if thread.is_alive():
+            window.after(100, check_thread)
+            show.set(f"轉換完成檔案數: {progress.get()} / {num}")
+        else:
+            bar["value"] = num
+            show.set(f"轉換完成檔案數: {num} / {num}\n已轉換完成。")
+    # ---check function---
+    global num, progress, thread
+    thread = threading.Thread(target=export)
+    window = tk.Toplevel()
+    window.title("輸出進度")
+    window.minsize(width=200, height=100)
+    window.resizable(False, False)
+    bar = ttk.Progressbar(window, maximum=num, variable=progress)
+    bar.pack()
+    show = tk.StringVar()
+    show.set(f"轉換完成檔案數: 0 / {num}")
+    wp_1 = tk.Label(window, textvariable=show, font=("Microsoft JhengHei UI", 10))
+    wp_1.pack()
+
+    bar.start()
+    thread.start()
+    window.after(100, check_thread)
+
+    wb_1 = tk.Button(window, text="關閉所有視窗", command = close)
+    wb_1.pack()
+# ---new window function---
+
+# ---generating function---
+def export():
+    global p_numbers, p_paragraph, placeholders, p_title, save_with, save_name, save_folder, doc_path, data_path, output, num, progress, typeVar
     data = pd.read_excel(data_path)
-    for i in range(len(data[p_title[0]])):
+    for i in range(num):
+        doc = docx.Document(doc_path)
+        progress.set(i)
         for j in range(p_numbers):
             inline = doc.paragraphs[p_paragraph[j]].runs
             for k in range(len(inline)):
@@ -33,12 +74,12 @@ def export(pdf):
                     inline[k].text = text
         docx_name = save_folder + "/" + save_name + "_" + data[save_with][i].replace(" ", "_") + ".docx"
         doc.save(docx_name)
-        if pdf:
+        if typeVar.get():
             pdf_name = save_folder + "/"  + save_name + "_" + data[save_with][i].replace(" ", "_") + ".pdf"
             convert(docx_name, pdf_name)
             os.remove(docx_name)
     return None
-# ---generating functions---
+# ---generating function---
 
 # ---placeholder function---
 def find_words():
@@ -57,6 +98,7 @@ def select_doc():
     filetypes = (('document files', '*.docx'),)
     doc_path = fd.askopenfilename(title='Choose a file', initialdir='./', filetypes=filetypes)
     show_doc.set(doc_path.split("/")[-1])
+    output.set("")
     return None
 # ---choose doc function---
     
@@ -66,6 +108,7 @@ def select_data():
     filetypes = (('Excel files', '*.xlsx'),)
     data_path = fd.askopenfilename(title='Choose a file', initialdir='./', filetypes=filetypes)
     show_data.set(data_path.split("/")[-1])
+    output.set("")
     return None
 # ---choose data function---
     
@@ -84,9 +127,10 @@ def set_words():
         output.set(err)
         return None
     elif (p_numbers == len(placeholders) and p_numbers == len(p_title)):
+        output.set("")
         return None
     else:
-        err = "error: 3. 替代數量:" + str(p_numbers) + "，替代文字數量:" + str(len(placeholders)) + "，標題名稱數量:" + str(len(p_title)) + "。三者不相符。"
+        err = f"error: 3. 替代數量:{p_numbers}，替代文字數量:{len(placeholders)}，標題名稱數量:{len(p_title)}。三者不相符。"
         output.set(err)
         return None
 # ---set word function---
@@ -101,6 +145,7 @@ def set_save():
         output.set(err)
         return None
     elif save_with in p_title:
+        output.set("")
         return None
     else:
         err = "error: 4. 命名標題不存在於標題名稱中。"
@@ -117,7 +162,7 @@ def select_folder():
     
 # ---execute function---
 def execute():
-    global p_numbers, p_paragraph, placeholders, p_title, save_with, save_name, save_folder, doc_path, data_path, typeVar, output
+    global p_numbers, p_paragraph, placeholders, p_title, save_with, save_name, save_folder, doc_path, data_path, typeVar, output, num
     output.set("")
     #validity check
     if os.path.isfile(doc_path) == False:
@@ -133,7 +178,7 @@ def execute():
         output.set(err)
         return None
     if (p_numbers == len(placeholders) and p_numbers == len(p_title)) == False:
-        err = "error: 3. 替代數量:" + str(p_numbers) + "，替代文字數量:" + str(len(placeholders)) + "，標題名稱數量:" + str(len(p_title)) + "。三者不相符。"
+        err = f"error: 3. 替代數量:{p_numbers}，替代文字數量:{len(placeholders)}，標題名稱數量:{len(p_title)}。三者不相符。"
         output.set(err)
         return None
     if save_name == "" or save_with == "":
@@ -151,101 +196,103 @@ def execute():
     p_paragraph = find_words()
     for i in range(len(p_paragraph)):
         if p_paragraph[i] == 0:
-            err = "error: 範例檔案中該關鍵字不存在:" + placeholders[i]
+            err = "error: 範例檔案中該關鍵字不存在: " + placeholders[i]
             output.set(err)
             return None
     data = pd.read_excel(data_path)
     for i in range(len(p_title)):
         if p_title[i] not in data:
-            err = "error: 資料檔案中該標題名稱不存在:" + p_title[i]
+            err = "error: 資料檔案中該標題名稱不存在: " + p_title[i]
             output.set(err)
             return None
-    
-    export(typeVar.get())
-    output.set("產生完成。")
+    num = len(data[p_title[0]])
+    new()
     return None
 # ---execute function---
 
 # ---main function---
-window = tk.Tk()
-window.title("通知／證明產生器")
-window.minsize(width=300, height=600)
-window.resizable(False, False)
+root = tk.Tk()
+root.title("通知／證明產生器")
+root.minsize(width=300, height=600)
+root.resizable(False, False)
 
-p1 = tk.Label(text="1. 選取範本檔案(.docx):", font=("Microsoft JhengHei UI", 12))
+p1 = tk.Label(root, text="1. 選取範本檔案(.docx):", font=("Microsoft JhengHei UI", 12))
 p1.pack(anchor="w")
-b1 = tk.Button(text="選取檔案", command=select_doc)
+b1 = tk.Button(root, text="選取檔案", command=select_doc)
 b1.pack(anchor="w")
 show_doc = tk.StringVar()
-p1_1 = tk.Label(textvariable=show_doc, font=("Microsoft JhengHei UI", 10))
+p1_1 = tk.Label(root, textvariable=show_doc, font=("Microsoft JhengHei UI", 10))
 p1_1.place(x=60, y=28)
-pm_1 = tk.Label(text="", font=(10))
+pm_1 = tk.Label(root, text="", font=(10))
 pm_1.pack()
 
-p2 = tk.Label(text="2. 選取資料檔案(.xlsx):", font=("Microsoft JhengHei UI", 12))
+p2 = tk.Label(root, text="2. 選取資料檔案(.xlsx):", font=("Microsoft JhengHei UI", 12))
 p2.pack(anchor="w")
-b2 = tk.Button(text="選取檔案", command=select_data)
+b2 = tk.Button(root, text="選取檔案", command=select_data)
 b2.pack(anchor="w")
 show_data = tk.StringVar()
-p1_1 = tk.Label(textvariable=show_data, font=("Microsoft JhengHei UI", 10))
+p1_1 = tk.Label(root, textvariable=show_data, font=("Microsoft JhengHei UI", 10))
 p1_1.place(x=60, y=106)
-pm_2 = tk.Label(text="", font=(10))
+pm_2 = tk.Label(root, text="", font=(10))
 pm_2.pack()
 
-p3 = tk.Label(text="3. 設定關鍵字:", font=("Microsoft JhengHei UI", 12))
+p3 = tk.Label(root, text="3. 設定關鍵字:", font=("Microsoft JhengHei UI", 12))
 p3.pack(anchor="w")
-p3_1 = tk.Label(text="替代數量:", font=("Microsoft JhengHei UI", 10))
+p3_1 = tk.Label(root, text="替代數量:", font=("Microsoft JhengHei UI", 10))
 p3_1.pack(anchor="w")
-i3_1 = tk.Entry()
+i3_1 = tk.Entry(root)
 i3_1.place(x=60, y=185)
-p3_2 = tk.Label(text="替代文字:", font=("Microsoft JhengHei UI", 10))
+p3_2 = tk.Label(root, text="替代文字:", font=("Microsoft JhengHei UI", 10))
 p3_2.pack(anchor="w")
-i3_2 = tk.Entry()
+i3_2 = tk.Entry(root)
 i3_2.place(x=60, y=208)
-p3_3 = tk.Label(text="標題名稱:", font=("Microsoft JhengHei UI", 10))
+p3_3 = tk.Label(root, text="標題名稱:", font=("Microsoft JhengHei UI", 10))
 p3_3.pack(anchor="w")
-i3_3 = tk.Entry()
+i3_3 = tk.Entry(root)
 i3_3.place(x=60, y=231)
-b3 = tk.Button(text="儲存", command=set_words)
+b3 = tk.Button(root, text="儲存", command=set_words)
 b3.pack(anchor="w")
-pm_3 = tk.Label(text="", font=(10))
+pm_3 = tk.Label(root, text="", font=(10))
 pm_3.pack()
 
-p4 = tk.Label(text="4. 設定輸出檔案命名:", font=("Microsoft JhengHei UI", 12))
+p4 = tk.Label(root, text="4. 設定輸出檔案命名:", font=("Microsoft JhengHei UI", 12))
 p4.pack(anchor="w")
-p4_1 = tk.Label(text="檔案名稱:", font=("Microsoft JhengHei UI", 10))
+p4_1 = tk.Label(root, text="檔案名稱:", font=("Microsoft JhengHei UI", 10))
 p4_1.pack(anchor="w")
-i4_1 = tk.Entry()
+i4_1 = tk.Entry(root)
 i4_1.place(x=60, y=332)
-p4_2 = tk.Label(text="命名標題:", font=("Microsoft JhengHei UI", 10))
+p4_2 = tk.Label(root, text="命名標題:", font=("Microsoft JhengHei UI", 10))
 p4_2.pack(anchor="w")
-i4_2 = tk.Entry()
+i4_2 = tk.Entry(root)
 i4_2.place(x=60, y=355)
-b4_1 = tk.Button(text="選取儲存資料夾", command=select_folder)
+b4_1 = tk.Button(root, text="選取儲存資料夾", command=select_folder)
 b4_1.pack(anchor="w")
 show_folder = tk.StringVar()
-p4_3 = tk.Label(textvariable=show_folder, font=("Microsoft JhengHei UI", 10))
+p4_3 = tk.Label(root, textvariable=show_folder, font=("Microsoft JhengHei UI", 10))
 p4_3.place(x=100, y=378)
-b4_2 = tk.Button(text="儲存", command=set_save)
+b4_2 = tk.Button(root, text="儲存", command=set_save)
 b4_2.pack(anchor="w")
-pm_4 = tk.Label(text="", font=(10))
+pm_4 = tk.Label(root, text="", font=(10))
 pm_4.pack()
 
-p5 = tk.Label(text="5. 選擇輸出檔案類型:", font=("Microsoft JhengHei UI", 12))
+p5 = tk.Label(root, text="5. 選擇輸出檔案類型:", font=("Microsoft JhengHei UI", 12))
 p5.pack(anchor="w")
 typeVar = tk.BooleanVar()
-radio1 = tk.Radiobutton(text=".docx檔", variable=typeVar, value=False)
-radio2 = tk.Radiobutton(text=".pdf檔", variable=typeVar, value=True)
+radio1 = tk.Radiobutton(root, text=".docx檔", variable=typeVar, value=False)
+radio2 = tk.Radiobutton(root, text=".pdf檔", variable=typeVar, value=True)
 radio1.place(x=0, y=480)
 radio2.place(x=80, y=480)
-pm_5 = tk.Label(text="\n", font=(10))
+pm_5 = tk.Label(root, text="\n", font=(10))
 pm_5.pack()
 
-b6 = tk.Button(text="開始執行", command=execute)
+b6 = tk.Button(root, text="開始執行", command=execute)
 b6.pack()
 output = tk.StringVar()
-p6 = tk.Label(textvariable=output, font=("Microsoft JhengHei UI", 10))
+p6 = tk.Label(root, textvariable=output, font=("Microsoft JhengHei UI", 10), fg='red')
 p6.pack(anchor="w")
 
-window.mainloop()
+progress = tk.IntVar()
+progress.set(0)
+
+root.mainloop()
 # ---main function---
